3d343d5dd1717126ef9b68b77db0160f7c65f05f
Atl4s <nazarenko1996@gmail.com>
2016-04-22 19:20:28 +0300

    отзывы теперь работают

--- phantom.com/js/init.js
+++ phantom.com/js/init.js
@@ -1,22 +1,46 @@
-(function($){
   $(function(){
-
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+ });
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
- $(document).ready(function(){
       $('.slider').slider({full_width: true});
-    });
-$(document).ready(function(){
-    $('.scrollspy').scrollSpy();
-  });
 
- $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+    
     $('.materialboxed').materialbox();
-  });
-$(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+
+
     $('.modal-trigger').leanModal();
-  });
+
+
+    $("#send").click(function(){
+        var name = $("#name").val();
+        var lastname = $("#last_name").val();
+        var feedback = $("#feedback").val();
+        if(name === '' || lastname === '' || feedback === '')
+        {
+        alert('Заполните все поля!');
+        return false;
+        }
+        $(".feedbacks").append("<li class='collection-item avatar'><img src='noname.png' alt='' class='circle'><span class='title'></span><span class='close'>x</span><span class='like'><i class='material-icons'>thumb_up</i></span><p></p></li>");
+        $(".title:last").text(name+' '+lastname);
+        $("p:last").text('«'+feedback+'»');
+        $("#name").val('');
+        $("#last_name").val('');
+        $("#feedback").val('');
+    });
+   
+
+$(document).ready(function () {
+    $('html').on('click','.close', function () {                               
+        $(this).parent().remove();    
+    });
+    
+     $('html').on('click','.like', function () {                               
+         if ($(this).css('color') == 'rgb(170, 170, 170)'){
+        $(this).css('color','teal');}
+        else if ($(this).css('color') == 'rgb(0, 128, 128)'){
+        $(this).css('color','#aaa');}    
+    });
+});
+
